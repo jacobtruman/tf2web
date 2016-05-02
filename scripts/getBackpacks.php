f543884340a9081432f1ae6194243dbf5dcfc6a2
Jacob Truman <jatruman@adobe.com>
2016-05-02 07:33:53 -0600

    Fix relative paths

--- scripts/getBackpacks.php
+++ scripts/getBackpacks.php
@@ -1,8 +1,8 @@
 #!/usr/bin/php
 <?php
 
-require_once("../classes/TF2Backpack.class.php");
-$config_file = "../config.json";
+require_once(dirname(__FILE__)."/../classes/TF2Backpack.class.php");
+$config_file = dirname(__FILE__)."/../config.json";
 $config = json_decode(file_get_contents($config_file), true);
 
 $accounts = $config['admin_accounts'];
